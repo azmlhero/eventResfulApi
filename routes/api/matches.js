c323aff9d397020fcce3b9cb36043b828781c3ba
ammar <azml1@gmail.com>
2021-06-25 18:08:33 +0500

    Update matches.js

--- routes/api/matches.js
+++ routes/api/matches.js
@@ -1,54 +1,27 @@
-var express = require('express');
-var router = express.Router();
-var mongoose = require('mongoose');
+let router = express.Router();
+let Match = require('../models/matchSchema');
 
-mongoose.connect('mongodb://localhost:27017/testproject',
-    {useNewUrlParser: true})
-    .then(()=>{
-        console.log("Database connected")
+router.post('/match', async (req, res) => {
+    let match = new Match({
+        city: req.body.city,
+        date: req.body.date,
+        teamA: req.body.teamA,
+        teamB: req.body.teamB
+    })
+    await match.save().then(() => {
+        res.sendStatus(200);
     }).catch(()=> {
-    console.log("Unable to connect to db")
-})
-
-
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.get("/events", function(req,res)
-{
-    let matches = eventschema.find();
-    matches
-        .then((result)=> {
-        res.send(result);
-    }).catch((err)=> {
-        res.send(err);
+        res.sendStatus(409);
     })
 })
 
-
-
-
-// write json file in postman and test it postman raw menu slect JSON option 
-router.post('/events', (req, res ) => {
-  let data = req.body;
-
-  if (data.teamB === data.teamA) {
-    return res.status(400).send({
-      message: "match should be between different teams"
-    }); 
-  }
-
-  let slot = await MatchModel.create({
-    date: new Date(new Date(data.date).getDate()),
-    city: data.city,
-    teamA: data.teamA,
-    teamB: data.teamB,
-  });
-
-  res.status(201).send(slot);
-
+router.get('/matches', async(req, res) => {
+    Match.find().then(result => {
+        res.send(result);
+    }).catch(err => {
+        console.log(err);
+        res.sendStatus(404);
+    })
 })
+
 module.exports = router;
